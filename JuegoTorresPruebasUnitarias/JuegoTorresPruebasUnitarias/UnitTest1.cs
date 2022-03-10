14eeb975f9e10b0ced7c7c60f5b98e6e7ef0ffce
Viktortilla <victormanuelvtr62@gmail.com>
2022-03-09 21:59:49 -0500

    subir carpeta
    si

---
+++ JuegoTorresPruebasUnitarias/JuegoTorresPruebasUnitarias/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JuegoTorresPruebasUnitarias
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
