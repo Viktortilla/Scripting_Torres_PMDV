90fcb9606991a44444cbda628b3f25028549dd25
Pietrovp1001 <pietro.villano@upb.edu.co>
2022-03-09 21:07:26 -0500

    Subir proyecto

---
+++ PruebaUnitarias_JuegoTorres/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaUnitarias_JuegoTorres
+{
+    internal class Test
+    {
+        Jugador joogador = new Jugador(true , Dictionary<2,8>);
+    }
+}
---
+++ PruebaUnitarias_JuegoTorres/Torre.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaUnitarias_JuegoTorres
+{
+    abstract class Torre
+    {
+        bool Vivo = true;
+        Dictionary<int, int> ValoresTorre = new Dictionary<int, int>();
+
+        public Torre(bool vivo, Dictionary<int, int> valoresTorre)
+        {
+            Vivo = vivo;
+            ValoresTorre = valoresTorre;
+        }
+        
+    }
+}
