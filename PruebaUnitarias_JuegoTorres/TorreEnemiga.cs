82294c24bcabde90eb3c4d8c5c52a4eb846cb705
Pietrovp1001 <pietro.villano@upb.edu.co>
2022-03-09 21:18:27 -0500

    Revert "Revert "Subir proyecto""
    This reverts commit cce3d265373dcda1c7b75a032d12632bc8b8ee3c.

---
+++ PruebaUnitarias_JuegoTorres/TorreEnemiga.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaUnitarias_JuegoTorres
+{
+    internal class TorreEnemiga : Torre
+    {
+        public TorreEnemiga(bool vivo, Dictionary<int, int> valoresTorre) : base(vivo, valoresTorre)
+        {
+            
+        }
+    }
+}
